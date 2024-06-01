5bc4152d53cbf667a15f655bc236139e994ceeb4
ShinhyukPark <parkgodhyuk@gmail.com>
2024-06-01 22:41:43 +0900

    Initial Commit

---
+++ URLSession/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  URLSession
+//
+//  Created by Shinhyuk on 2024/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        //.
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
