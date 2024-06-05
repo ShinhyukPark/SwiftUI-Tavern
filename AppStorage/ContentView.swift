c2876d0fc703d91d2e4b3f45cc7e09d7b841331c
ShinhyukPark <parkgodhyuk@gmail.com>
2024-06-05 23:47:29 +0900

    Initial Commit

---
+++ AppStorage/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AppStorage
+//
+//  Created by Shinhyuk on 2024/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
